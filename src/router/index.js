388de6e6b903e15b59ea8b99148d9f70ee12eb15
joan <joan.valdez@uvic.cat>
2026-06-03 01:08:30 +0200

    chore(router): add basic vue-router setup with home/climate/esg/footprint routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ClimateView from '../views/ClimateView.vue'
-import FootprintView from '../views/FootprintView.vue'
 import ESGView from '../views/ESGView.vue'
+import FootprintView from '../views/FootprintView.vue'
+
+const routes = [
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/climate', name: 'climate', component: ClimateView },
+  { path: '/esg', name: 'esg', component: ESGView },
+  { path: '/footprint', name: 'footprint', component: FootprintView },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/climate',
-      name: 'climate',
-      component: ClimateView
-    },
-    {
-      path: '/footprint',
-      name: 'footprint',
-      component: FootprintView
-    },
-    {
-      path: '/esg',
-      name: 'esg',
-      component: ESGView
-    }
-  ]
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
